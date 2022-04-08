b0554f8daead1fc1272cd3f27b75ed1822a59c9c
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-04-09 06:08:05 +0700

    [ADD] Draft Home page

---
+++ components/Home/BannerCarousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-carousel hide-delimiters cycle>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+})
+</script>
+<style></style>
---
+++ components/Home/RecommendList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="n in 12" :key="n" class="d-flex child-flex" cols="2">
+        <v-hover #default="{ hover }">
+          <v-card hover>
+            <v-sheet
+              v-if="hover"
+              class="tw-absolute tw-z-[2] tw-opacity-20"
+              color="primary"
+              height="100%"
+              width="100%"
+            ></v-sheet>
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+<style></style>
