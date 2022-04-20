f7fbccb3999823962c19abb0a1a2892d6c2d3236
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-04-21 03:23:45 +0700

    [UPDATE] newestList display

--- components/Home/NewestList.vue
+++ components/Home/NewestList.vue
@@ -15,7 +15,22 @@
         cols="2"
       >
         <v-hover #default="{ hover }">
-          <v-card hover>
+          <v-card hover @click="routeTo(`/posts/${post._id}`)">
+            <v-fab-transition>
+              <div class="tw-absolute tw-right-0 tw-m-2 tw-z-[3]" v-if="hover">
+                <v-btn
+                  class="tw-z-[3]"
+                  color="white"
+                  fab
+                  small
+                  depressed
+                  @click.stop="favoritePost('test')"
+                >
+                  <!-- <v-icon color="red">mdi-cards-heart</v-icon> -->
+                  <v-icon color="red">mdi-cards-heart-outline</v-icon>
+                </v-btn>
+              </div>
+            </v-fab-transition>
             <v-sheet
               v-if="hover"
               class="tw-absolute tw-z-[2] tw-opacity-20"
@@ -23,7 +38,7 @@
               height="100%"
               width="100%"
             ></v-sheet>
-            <v-img :src="post.images" aspect-ratio="1" class="grey lighten-2">
+            <v-img :src="post.imageUrl" aspect-ratio="1" class="grey lighten-2">
               <template v-slot:placeholder>
                 <v-row class="fill-height ma-0" align="center" justify="center">
                   <v-progress-circular
@@ -44,9 +59,9 @@
 import Vue from 'vue'
 import mixins from 'vue-typed-mixins'
 import { Post } from '~/interfaces/Post.interface'
-import post from '~/mixins/post'
+import posts from '~/mixins/posts'
 import utils from '~/mixins/utils'
-export default mixins(post, utils).extend({
+export default mixins(posts, utils).extend({
   async mounted() {
     this.newestPost = await this.getNewestPost()
   },
