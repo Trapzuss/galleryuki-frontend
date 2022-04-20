e160427368aecc1f0a874e24dce5cbcb14e7dec5
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-04-21 03:24:19 +0700

    [UPDATE] Profile menu layout

--- components/Layout/ProfileMenu.vue
+++ components/Layout/ProfileMenu.vue
@@ -10,23 +10,43 @@
     </template>
     <v-card>
       <v-list-item-content class="justify-start">
-        <div class="tw-mx-auto text-left">
+        <div class="tw-mx-auto text-left" v-if="isHaveUser">
           <div class="tw-mx-4">
             <v-avatar color="#f6f6f6" class="tw-my-2" rounded="pill">
               <v-icon color="#272727">mdi-account</v-icon>
             </v-avatar>
-            <h3>{{ user.fullName }}</h3>
+            <h3>{{ user.userDisplayName }}</h3>
             <p class="text-caption mt-1">
-              {{ user.email }}
+              {{ user.username }}
             </p>
           </div>
+
           <v-divider class="my-3"></v-divider>
-          <v-btn depressed text width="100%" class="d-flex justify-start">
-            Edit Account
-          </v-btn>
-          <v-divider class="my-3"></v-divider>
-          <v-btn depressed text width="100%" class="d-flex justify-start">
+          <v-btn
+            depressed
+            text
+            width="100%"
+            class="d-flex justify-start"
+            @click="$emit('onDisconnect')"
+          >
             Disconnect
+          </v-btn>
+        </div>
+        <div class="tw-mx-auto text-left" v-else>
+          <v-avatar color="#f6f6f6" class="tw-my-2" rounded="pill">
+            <v-icon color="#272727">mdi-account-alert</v-icon>
+          </v-avatar>
+          <span>You haven't logging in yet</span>
+
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            depressed
+            text
+            width="100%"
+            class="d-flex justify-start"
+            @click="routeTo('login')"
+          >
+            Login!
           </v-btn>
         </div>
       </v-list-item-content>
@@ -36,14 +56,29 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
+import mixins from 'vue-typed-mixins'
+import { User } from '~/interfaces/User.interface'
+import utils from '~/mixins/utils'
+export default mixins(utils).extend({
+  props: {
+    user: Object as Vue.PropType<User>,
+  },
+  computed: {
+    isHaveUser(): any {
+      try {
+        return this.user?._id
+      } catch (error) {
+        return {}
+      }
+    },
+  },
   data() {
     return {
-      user: {
-        initials: 'JD',
-        fullName: 'John Doe',
-        email: 'john.doe@doe.com',
-      },
+      // user: {
+      //   initials: 'JD',
+      //   fullName: 'John Doe',
+      //   email: 'john.doe@doe.com',
+      // },
     }
   },
 })
